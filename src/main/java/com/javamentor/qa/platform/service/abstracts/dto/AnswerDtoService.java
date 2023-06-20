2ca620bc9814d1580696e92ec288e3f9257dddcb
Владимир Походня <pokhodnya@ngs.ru>
2023-06-20 09:17:43 +0000

    Resolve "Разработать api получения списка DTO ответов по id вопроса #18"

--- src/main/java/com/javamentor/qa/platform/service/abstracts/dto/AnswerDtoService.java
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/dto/AnswerDtoService.java
@@ -1,4 +1,10 @@
 package com.javamentor.qa.platform.service.abstracts.dto;
 
+import com.javamentor.qa.platform.models.dto.AnswerDto;
+
+import java.util.List;
+
 public interface AnswerDtoService {
+    List<AnswerDto> getAllAnswersDtoByQuestionId(Long id, Long userId);
+
 }
--- src/main/java/com/javamentor/qa/platform/service/impl/dto/AnswerDtoServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/dto/AnswerDtoServiceImpl.java
@@ -1,9 +1,24 @@
 package com.javamentor.qa.platform.service.impl.dto;
 
+import com.javamentor.qa.platform.dao.abstracts.dto.AnswerDtoDao;
+import com.javamentor.qa.platform.models.dto.AnswerDto;
 import com.javamentor.qa.platform.service.abstracts.dto.AnswerDtoService;
+import com.javamentor.qa.platform.service.abstracts.model.QuestionService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AnswerDtoServiceImpl implements AnswerDtoService {
+    private final AnswerDtoDao answerDtoDao;
+
+    public AnswerDtoServiceImpl(AnswerDtoDao answerDtoDao) {
+        this.answerDtoDao = answerDtoDao;
+    }
+
+    @Override
+    public List<AnswerDto> getAllAnswersDtoByQuestionId(Long id, Long userId) {
+        return answerDtoDao.getAllAnswersDtoByQuestionId(id, userId);
+    }
 
 }
