513b85863bbfc56a5e156e9825e3588971044b78
Антон Коробов <rexsitael@gmail.com>
2023-06-23 08:25:05 +0000

    Resolve "Разработать api который возвращает лист, содержащий топ-10 тегов #42"

--- src/main/java/com/javamentor/qa/platform/dao/impl/dto/TagDtoDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/dto/TagDtoDaoImpl.java
@@ -1,26 +1,29 @@
 package com.javamentor.qa.platform.dao.impl.dto;
 
+
 import com.javamentor.qa.platform.dao.abstracts.dto.TagDtoDao;
-import com.javamentor.qa.platform.dao.abstracts.repository.ReadWriteDao;
-import com.javamentor.qa.platform.dao.impl.repository.ReadWriteDaoImpl;
-import com.javamentor.qa.platform.models.dto.TagDto;
-import com.javamentor.qa.platform.models.dto.UserDto;
-import com.javamentor.qa.platform.models.entity.question.Tag;
-import com.javamentor.qa.platform.service.impl.repository.ReadWriteServiceImpl;
+import com.javamentor.qa.platform.models.dto.RelatedTagDto;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Repository
-public class TagDtoDaoImpl extends ReadWriteDaoImpl<TagDto, Long> implements TagDtoDao {
-
+public class TagDtoDaoImpl implements TagDtoDao {
     @PersistenceContext
     private EntityManager entityManager;
 
-
-
-
+    @Override
+    public List<RelatedTagDto> getTopTags() {
+        return entityManager.createQuery("""
+                            SELECT new com.javamentor.qa.platform.models.dto.RelatedTagDto(t.id, t.name, COUNT(q))
+                            FROM Tag t
+                            INNER JOIN t.questions q
+                            GROUP BY t.id, t.name
+                            ORDER BY COUNT(q) DESC
+                        """, RelatedTagDto.class)
+                .setMaxResults(10).getResultList();
+    }
 }
---
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceTagController.java
@@ -0,0 +1,37 @@
+package com.javamentor.qa.platform.webapp.controllers.rest;
+
+import com.javamentor.qa.platform.models.dto.RelatedTagDto;
+import com.javamentor.qa.platform.service.abstracts.dto.TagDtoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/user/tag/")
+@Api(tags = "Related Tag Api")
+public class ResourceTagController {
+
+    private final TagDtoService tagDtoService;
+
+    public ResourceTagController(TagDtoService tagDtoService) {
+        this.tagDtoService = tagDtoService;
+    }
+
+    @GetMapping("related")
+    @ApiOperation(value = "Запросить список популярных тегов", notes = "Возвращает 10 самых часто используемых тегов")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "список самых популярных тегов вопрос успешно получен"),
+            @ApiResponse(code = 400, message = "плохой запрос")
+    })
+    public ResponseEntity<List<RelatedTagDto>> getTop10Tags() {
+        return new ResponseEntity<>(tagDtoService.getTopTags(), HttpStatus.OK);
+    }
+}
